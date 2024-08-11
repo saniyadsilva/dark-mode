7cfb68bbdde9d1bd105f5272a09d1b79c583c517
sani <saniya@gmail.com>
2024-08-11 13:59:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
-// import TextForm from './components/TextForm';
+import TextForm from './components/TextForm';
+import React, {useState} from 'react';//imrs
+
 function App() {
+  const [mode, setMode] = useState('light');
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor ='#343a40';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor ='white';
+    }
+  }
   return (
     <>
-<Navbar title ="textUtils"/>
+<Navbar title ="textUtils" mode={mode} toggleMode={toggleMode}/>
 <div className="container my-3">
-{/* <TextForm heading = "enter the text to analyze"/> */}
-<About/>
+<TextForm heading = "enter the text to analyze" mode={mode}/>
+{/* <About/> */}
 </div>
 </>
 );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -22,23 +22,31 @@ export default function TextForm(props) {
     setText(event.target.value);
   }
 
+  const handleCopy = () =>{
+    console.log("i am copy");
+    var text=document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  }
+
   const [text, setText] = useState('Enter text here');
   return (
     <>
-    <div className="container">  
+    <div className="container" style={{color:props.mode==='dark'?'white':'black'}} >  
       <h1>{props.heading}</h1>
       <div className="mb-3">
-      <textarea className="form-control" value={text} onChange={HandleOnChange} id="myBox" rows="8"></textarea>
+      <textarea className="form-control" value={text} onChange={HandleOnChange} style={{backgroundColor:props.mode==='dark'?'#343a40':'white', color:props.mode==='dark'?'white':'black'}}  id="myBox" rows="8"></textarea>
       </div>
       <button className="btn btn-primary mx-3" onClick={handleUpClick}>convert to Uppercase</button>
       <button className="btn btn-primary mx-2" onClick={handleLoClick}>convert to Lowercase</button>
       <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
+      <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy Text</button>
     </div>
-    <div className="container my-3">
+    <div className="container my-3" style={{color:props.mode==='dark'?'white':'black'}} >
       <h1>your text summary </h1>
       <p>{text.split (" ").length} words and {text.length} characters</p>
       <h2>preview</h2>
-      <p>{text}</p>
+      <p>{text.length>0?text:"Enter something to preview it here"}</p>
     </div>
     </>
   );
